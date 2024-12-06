8ac006a4e166c93113b5a98332efaf8fa2d87ea3
RickardCardell <rickard.cardell@live.com>
2024-12-06 16:05:19 +0100

    Remove intro

--- setup/00-intro.sh
+++ setup/00-intro.sh
@@ -166,7 +166,7 @@ else
 
     export SUBSCRIPTION_ID=$(az account show --query id -o tsv)
 
-    az ad sp create-for-rbac --sdk-auth --role Owner --scopes /subscriptions/$SUBSCRIPTION_ID | tee azure-creds.json
+    #az ad sp create-for-rbac --sdk-auth --role Owner --scopes /subscriptions/$SUBSCRIPTION_ID | tee azure-creds.json
 
     kubectl --namespace crossplane-system create secret generic azure-creds --from-file creds=./azure-creds.json
 
